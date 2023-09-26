11c9a908f01ccef818a903761ea694163f885c37
Marcus Mäkilä <68429681+Emphelito@users.noreply.github.com>
2023-09-27 00:01:09 +0200

    Update predict.cpp

--- LAB3/predict.cpp
+++ LAB3/predict.cpp
@@ -1,40 +1,38 @@
 // Predict method to predict class labels for test data
 std::vector<double> LogisticRegression::predict(const std::vector<std::vector<double>>& X_test) {
     std::vector<double> predictions;
-    
-    /* Implement the following:
-    	--- Loop over each test example
-        --- Add bias term to the test example
-        --- Calculate scores for each class by computing the weighted sum of features
-        --- Predict class label with the highest score
-    */
-    //random number generator for the weights
-    std::random_device rand;
-    std::mt19937 generate(rand());
-    std::uniform_real_distribution<double> dist(-1.0, 1.0);
-    // TODO
-    for (std::vector<double> test : X_test) {
-        double score = 0;
-        int count = 0;
-        std::vector<double> weights(test.size());
-        if (count == 0) {
-            for (int i = 0; i < test.size(); i++) {
-                weights[i] = dist(generate);
+        //loop through all vectors in X_test
+        for (const std::vector<double>& sample : X_test) {
+            std::vector<double> class_probabilities;
+            //Loop through all classes in weights vector
+            for (int class_label = 1; class_label <= weights.size(); ++class_label) {
+                //Calculate the weighted sum of features
+                double weighted_sum = 0.0;
+                for (double j = 0; j < sample.size(); ++j) {
+                    weighted_sum += weights[class_label - 1][j] * sample[j];
+                }
+
+                //Calculate the sigmoid of the weighted sum and store it
+                double sigmoid_output = sigmoid(weighted_sum);
+                class_probabilities.push_back(sigmoid_output);
             }
-            count = 1;
+
+            /*Find the class with the highest probability and assign it as the prediction
+            
+            If the if case is not fufilled that mean that the predicted class is equal to 1
+            thats why class_label is set to two rather than 1 */
+            int predicted_class = 1;
+            double max_probability = class_probabilities[0];
+            for (int class_label = 2; class_label <= weights.size(); ++class_label) {
+                if (class_probabilities[class_label - 1] > max_probability) {
+                    predicted_class = class_label;
+                    max_probability = class_probabilities[class_label - 1];
+                }
+            }
+
+            //Store the predicted classes (1, 2, 3) in the predictions vector
+            predictions.push_back(static_cast<double>(predicted_class));
         }
 
-        for (int i = 0; i < test.size(); i++) {
-            score = score + weights[i] * test[i];
-        }
-
-        double predicted_class_label;
-        if (score >= 0.0) {
-            predicted_class_label = 1.0;
-        }
-        else {
-            predicted_class_label = 0.0;
-        }
-    }
-    return predictions;
+        return predictions;
 }
